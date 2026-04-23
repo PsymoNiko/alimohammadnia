a1130bcff53808f991cfa060afb7aa4fe0100227
Ali Mohammadnia <alimohammadnia127@gmail.com>
2026-04-23 13:18:29 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  // Only add basePath/assetPrefix when building for GitHub Pages
-  basePath: isGithubPages ? '/alimohammadnia' : '',
-  assetPrefix: isGithubPages ? '/alimohammadnia' : '',
+  images: { unoptimized: true },
+  basePath: '/alimohammadnia',
+  assetPrefix: '/alimohammadnia',
 };
 
 export default nextConfig;
